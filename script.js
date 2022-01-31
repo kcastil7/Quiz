5be0830759f7403cae9f3746249e368f19dd92f5
Kevin <kcas51@yahoo.com>
2022-01-31 11:50:42 -0800

    Added timer and wrong message when user selects wrong answer

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ var quizContent = document.querySelector(".quiz-content");
 var question = document.querySelector(".question");
 var ansBtnEl = document.querySelector(".ans-bts");
 var ansBtn = document.querySelector(".ans-btn");
+var scoreEl = document.querySelector(".timer");
 var btn
 var quesCounter = 0;
+var timeCounter = 60;
 
 
 startBtn.addEventListener('click', function () {
-    console.log("eventlistener activated");
+    // console.log("eventlistener activated");
     defaultContent.textContent = "";
     quizContent.style.display = "block";
 
@@ -39,24 +41,61 @@ startBtn.addEventListener('click', function () {
     displayQuestion();
 
     function displayQuestion() {
+        var timescore = setInterval(function() {
+            if(timeCounter > 0) {
+            scoreEl.textContent = "Score: " + timeCounter;
+            timeCounter--;
+            }
+            else {
+                clearInterval(timescore);
+                scoreEl.textContent = "Time is Up!";
+                timeCounter--;
+            }
+        },1000);
+
         ansBtnEl.textContent = "";
         question.textContent = questions[quesCounter].ques;
         for (var i = 0; i < 4; i++) {
             btn = document.createElement("button");
             btn.setAttribute("class", "ans-btn");
             btn.setAttribute("is-correct", questions[quesCounter].answer[i].isCorrect);
-            console.log(questions[quesCounter].answer[i].isCorrect);
+            // console.log(questions[quesCounter].answer[i].isCorrect);
             btn.textContent = questions[quesCounter].answer[i].ansText;
             ansBtnEl.appendChild(btn);
         }
+
     }
 
     ansBtnEl.addEventListener('click', function (event){ 
         var ansSubmit = event.target;
-        console.log (ansSubmit.getAttribute("is-correct"));
-        
+        var isCorrect = (ansSubmit.getAttribute("is-correct") === "true");
+        if(isCorrect){
+            // console.log(ansSubmit.getAttribute("is-correct"));
+            quesCounter++;
+            displayQuestion();
+        }
+        else {
+            var wrong = document.querySelector("#wrong-alert");
 
 
+            if (wrong ===null){
+                var wrongtText = document.createElement("p");
+                wrongtText.id = "wrong-alert";
+                wrongtText.textContent = "Wrong!";
+                ansBtnEl.append(wrongtText);
+                timeCounter = timeCounter-10;
+            }
+
+            else {
+                wrong.remove();
+                var wrongtText = document.createElement("p");
+                wrongtText.id = "wrong-alert";
+                wrongtText.textContent = "Wrong!";
+                ansBtnEl.append(wrongtText);
+                timeCounter = timeCounter-10;
+
+            }
+        }
     });
 
 
